82332d179a7655560af46452ead22b93eb810a5a
yabertra <yannis.bertrand@outlook.be>
2025-05-23 09:56:36 +0200

    Included possibility to specify noise_threshold parameter of inductive miner for clean log creation and ran the clean log creation script for thresholds 0.1, 0.2, 0.3, 0.4, 0.5

--- GT_log_creation/trace_by_trace_filtering.py
+++ GT_log_creation/trace_by_trace_filtering.py
@@ -52,7 +52,7 @@ def filter_perfect_fitting_cases_one_by_one(
     return df_log[df_log[case_id_col].isin(perfect_cases)].copy()
 
 
-def do_slow_filtering(logname, zipfile=False):
+def do_slow_filtering(logname, noise_threshold= 0.2, zipfile=False):
     """
     Perform slow filtering of the event log to keep only perfectly fitting cases.
 
@@ -63,24 +63,25 @@ def do_slow_filtering(logname, zipfile=False):
     """
     # Load the event log
     if zipfile:
-        log = pm4py.read_xes('GT_log_creation/original_event_logs/' + logname + '.xes.gz')
+        log = pm4py.read_xes('original_event_logs/' + logname + '.xes.gz')
     else:
-        log = pm4py.read_xes('GT_log_creation/original_event_logs/' + logname + '.xes')  # Replace with your log file path
+        log = pm4py.read_xes('original_event_logs/' + logname + '.xes')  # Replace with your log file path
 
     # Discover the process model
-    net, im, fm = utils.discover_net(log, algo='inductive', noise_threshold=0.2)
+    net, im, fm = utils.discover_net(log, algo='inductive', noise_threshold=noise_threshold)
 
     # Save the process model
-    pm4py.write_pnml(net, im, fm, 'GT_log_creation/process_models/' + logname + '_inductive.pnml')  # Replace with your desired output path
+    pm4py.write_pnml(net, im, fm, 'process_models/' + logname + '_' + str(noise_threshold).replace('.', '_') + '_inductive.pnml')  # Replace with your desired output path
 
     # Show view of the process model
-    pm4py.save_vis_petri_net(net, im, fm, file_path='GT_log_creation/process_models/'+logname+'_inductive_view.png')  # This will save a view for the Petri net
+    pm4py.save_vis_petri_net(net, im, fm, file_path='process_models/' + logname + '_' + str(noise_threshold).replace('.', '_') +'_inductive_view.png')  # This will save a view for the Petri net
 
     # Filter the log to keep only the perfectly fitting cases
     new_log = filter_perfect_fitting_cases_one_by_one(log, net, im, fm)
 
     # Save the filtered log
-    pm4py.write_xes(new_log, 'GT_log_creation/cleaned_event_logs/' + logname + '_perfect_fitting_cases.xes')  # Replace with your desired output path   
+    print(len(new_log))
+    pm4py.write_xes(new_log, 'cleaned_event_logs/' + logname + '_' + str(noise_threshold).replace('.', '_') + '_perfect_fitting_cases.xes')  # Replace with your desired output path
    
 
 '''
@@ -91,7 +92,40 @@ do_slow_filtering('Sepsis', zipfile=True)
 do_slow_filtering('RTFM', zipfile=False)
 '''
 
-do_slow_filtering('BPI_Challenge_2012', zipfile=True)
+# Apply the filtering to all logs with different thresholds (from 0.1 to 0.5)
 
-do_slow_filtering('HospitalBilling', zipfile=True)
-                     
+do_slow_filtering('BPI_Challenge_2012', noise_threshold=0.1, zipfile=True)
+do_slow_filtering('BPI_Challenge_2012', noise_threshold=0.2, zipfile=True)
+do_slow_filtering('BPI_Challenge_2012', noise_threshold=0.3, zipfile=True)
+do_slow_filtering('BPI_Challenge_2012', noise_threshold=0.4, zipfile=True)
+do_slow_filtering('BPI_Challenge_2012', noise_threshold=0.5, zipfile=True)
+
+do_slow_filtering('Hospital Billing - Event Log', noise_threshold=0.1, zipfile=True)
+do_slow_filtering('Hospital Billing - Event Log', noise_threshold=0.2, zipfile=True)
+do_slow_filtering('Hospital Billing - Event Log', noise_threshold=0.3, zipfile=True)
+do_slow_filtering('Hospital Billing - Event Log', noise_threshold=0.4, zipfile=True)
+do_slow_filtering('Hospital Billing - Event Log', noise_threshold=0.5, zipfile=True)
+
+do_slow_filtering('Sepsis Cases - Event Log', noise_threshold=0.1, zipfile=True)
+do_slow_filtering('Sepsis Cases - Event Log', noise_threshold=0.2, zipfile=True)
+do_slow_filtering('Sepsis Cases - Event Log', noise_threshold=0.3, zipfile=True)
+do_slow_filtering('Sepsis Cases - Event Log', noise_threshold=0.4, zipfile=True)
+do_slow_filtering('Sepsis Cases - Event Log', noise_threshold=0.5, zipfile=True)
+
+do_slow_filtering('Road_Traffic_Fine_Management_Process', noise_threshold=0.1, zipfile=True)
+do_slow_filtering('Road_Traffic_Fine_Management_Process', noise_threshold=0.2, zipfile=True)
+do_slow_filtering('Road_Traffic_Fine_Management_Process', noise_threshold=0.3, zipfile=True)
+do_slow_filtering('Road_Traffic_Fine_Management_Process', noise_threshold=0.4, zipfile=True)
+do_slow_filtering('Road_Traffic_Fine_Management_Process', noise_threshold=0.5, zipfile=True)
+
+# Converting the helpdesk log to an XES
+df_log = pd.read_csv('original_event_logs/' + 'Helpdesk - Event Log' + '.csv')
+df_log = pm4py.format_dataframe(df_log, case_id='Case ID', activity_key='Activity', timestamp_key='Complete Timestamp')
+log = pm4py.convert_to_event_log(df_log)
+pm4py.write_xes(log, 'original_event_logs/' + 'Helpdesk - Event Log' + '.xes')  # Replace with your desired output path
+
+do_slow_filtering('Helpdesk - Event Log', noise_threshold=0.1, zipfile=False)
+do_slow_filtering('Helpdesk - Event Log', noise_threshold=0.2, zipfile=False)
+do_slow_filtering('Helpdesk - Event Log', noise_threshold=0.3, zipfile=False)
+do_slow_filtering('Helpdesk - Event Log', noise_threshold=0.4, zipfile=False)
+do_slow_filtering('Helpdesk - Event Log', noise_threshold=0.5, zipfile=False)
